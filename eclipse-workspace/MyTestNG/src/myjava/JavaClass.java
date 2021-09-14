5d85495d9708934ee04b1aff1ad535d9720e2ccf
Shams4891 <shamstasvir091@gmail.com>
2021-09-13 22:50:10 -0400

    my test

---
+++ eclipse-workspace/MyTestNG/src/myjava/JavaClass.java
@@ -0,0 +1,14 @@
+package myjava;
+
+import java.util.LinkedList;
+
+public class JavaClass {
+   
+	
+	
+	  public static void main(String[] args) {
+		
+		  
+		   LinkedList linky = new LinkedList();
+	}
+}
